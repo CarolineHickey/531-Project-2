7e54769c052041e67e8ae4bb4bed94f5a57017b9
nrmill <70912745+nrmill@users.noreply.github.com>
2021-11-05 17:35:46 -0600

    complete script 1

--- scriptOne.ps1
+++ scriptOne.ps1
@@ -1,54 +1,61 @@
-#Get-Member -- shows info on theobject
-# Import active directory module for running AD cmdlets
-Import-Module activedirectory
+param(
+    [string]$source="c:\temp\GroupProject2Usernames.csv",
+    [string]$destination="c:\temp\destination.csv",
+    [switch]$output,
+    [switch]$help
+)
 
-#This is the path up until the passed in csv file:
-$CSVPartialPath = 'C:Users\Administrator\Downloads\'
-
-#param($name= <value>) -- then access it through $name
-#This is the variable assigned to the input of argument 0
-$CSVFile = $args[0] #GroupProject2Usernames.csv
-
-#This variabe is the rsult of concatonating the two above
-$CSVPath = $CSVPartialPath + $CSVFile
-
-#Store the data from ADUsers.csv in the $ADUsers variable
-$ADUsers = Import-csv $CSVPath #C:Users\Administrator\Downloads\GroupProject2Usernames.csv
-
-#Loop through each row containing user details in the CSV file 
-foreach ($User in $ADUsers)
+if ($help)
 {
-	#Read user data from each field in each row and assign the data to a variable as below
-		
-	$Username 	= $User.UserName
-	$Firstname 	= $User.FirstName
-	$Lastname 	= $User.LastName
-	#$Password 	= $User.password
-    #$Password = $User.Password
-
-
-	#Check to see if the user already exists in AD
-	if (Get-ADUser -F {SamAccountName -eq $Username})
-	{
-		 #If user does exist, give a warning
-		 Write-Warning "A user account with username $Username already exist in Active Directory."
-	}
-	else
-	{
-		#User does not exist then proceed to create the new user account
-		
-        #Account will be created in the OU provided by the $OU variable read from the CSV file
-		New-ADUser `
-            -SamAccountName $Username `
-            -Name "$Firstname $Lastname" `
-            -GivenName $Firstname `
-            -Surname $Lastname `
-            -Enabled $True `
-            -DisplayName "$Lastname, $Firstname" `
-
-            #Require users to users to change password at first logon
-            -AccountPassword (convertto-securestring $Password -AsPlainText -Force) -ChangePasswordAtLogon $True
-            
-	}
+    write-host "This script adds users from a .csv to the Active Directory. Please provide a source destination and an output destination along with the 'output' switch parameter if you would like a file describing the added users, or just the source destination if you don't."
 }
+else
+{
+    # Import active directory module for running AD cmdlets
+    Import-Module activedirectory
 
+    #Store the data from GroupProject2Usernames.csv in the $ADUsers variable
+    $ADUsers = Import-csv $source #C:temp\GroupProject2Usernames.csv
+
+    # create an array to store added users
+    $AddedUsers = @()
+
+    #Loop through each row containing user details in the CSV file 
+    foreach ($User in $ADUsers)
+    {
+	    #Store username variable from csv's user data
+	    $Username = $User.UserName
+
+	    #Check to see if the user already exists in AD
+	    if (Get-ADUser -F {SamAccountName -eq $Username})
+	    {
+		    #If user does exist, give a warning
+		    Write-Warning "A user account with username $Username already exist in Active Directory."
+	    }
+	    else
+	    {
+		    #User does not exist so proceed to create the new user account
+
+		    #Store first-, and last- name variables from csv's user data
+		    $Firstname 	= $User.FirstName
+		    $Lastname 	= $User.LastName
+
+		    #Account will be created in the OU provided by the $OU variable read from the CSV file
+		    New-ADUser `
+			-SamAccountName $Username `
+			-Name "$Firstname $Lastname" `
+			-GivenName $Firstname `
+			-Surname $Lastname `
+			-Enabled $True `
+			-DisplayName "$Lastname, $Firstname" `
+			-AccountPassword (convertto-securestring "pass1234" -AsPlainText -Force) -ChangePasswordAtLogon $True #Require users to users to change password at first logon
+
+		    $AddedUsers += Get-ADUser $Username | Select SAMAccountName, SID
+            }
+    }
+
+    #if output flag is true, export $AddedUsers to .csv
+    if ($output) {
+        $AddedUsers | Export-Csv -Path $destination -NoTypeInformation
+    }
+}
