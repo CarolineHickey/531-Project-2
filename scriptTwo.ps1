9ae2461c291805ed38674cc9d70ace4df370e4c1
Squarerootofpi <53194917+Squarerootofpi@users.noreply.github.com>
2021-11-04 22:13:45 -0600

    Added instructions and script two start

---
+++ scriptTwo.ps1
@@ -0,0 +1,24 @@
+# https://docs.microsoft.com/en-us/windows/win32/wmisdk/retrieving-class-or-instance-data
+# This has info on the how to use wsi basics etc...
+
+
+# Script 2 –Gathering Information from Computers in the Enterprise
+# The following are guidelines as you create this script:
+# You should primarily use WMI classes to gather information.  
+    # Quick teaching moment here: Windows has been around for decades, and if you look at the 
+    # PowerPoint I uploaded in a previous class as optional reading called PowerShellDay 3, 
+    # it shows how there is something called the WMI Architecture which shows you can access 
+    # many of the objects in the windows operating system, regardless ofwhen/how they were 
+    # created over the years, through the WMI Scripting API.Do a Google search and briefly learn about WMI classes and accessing WMI using PowerShell.
+
+# Your script should be as comprehensiveas possible.
+# Example information may include BIOS information, programs installed on the computer, current programs running, accounts that exist on the computer, etc.
+# While the script by default should run on the machine where it is executed, it should also be able to be executed for a remote machine(assuming you have rights).
+
+
+# Your script should be able to be called from a PowerShellconsole and appropriate parameters passed to it.
+# Feel free to usethe internet to find potential solutions;however, you should not copy scripts exactly as you find them online.  You should write them yourself but can be guided by what you find. 
+# Make sure that you comment your script.
+# Your script should save the outputof the information that it gathersto a file.You can determine the type and organization of the file.
+
+
