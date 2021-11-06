a16945725cede6e625fb361cf0fc42895911c21e
nrmill <70912745+nrmill@users.noreply.github.com>
2021-11-05 18:09:09 -0600

    draft of script 2

--- scriptTwo.ps1
+++ scriptTwo.ps1
@@ -1,52 +1,42 @@
-# https://docs.microsoft.com/en-us/windows/win32/wmisdk/retrieving-class-or-instance-data
-# This has info on the how to use wsi basics etc...
+param(
+    [string]$ip=".",
+    [switch]$help
+)
 
+if ($help)
+{
+    write-host "This script gathers information on a computer from a given IP Address and outputs it to a .txt file. Please provide an IP Address."
+}
+else
+{
+    #create an array to store gathered information
+    $Info = @()
 
-# Script 2 –Gathering Information from Computers in the Enterprise
-# The following are guidelines as you create this script:
-# You should primarily use WMI classes to gather information.  
-    # Quick teaching moment here: Windows has been around for decades, and if you look at the 
-    # PowerPoint I uploaded in a previous class as optional reading called PowerShellDay 3, 
-    # it shows how there is something called the WMI Architecture which shows you can access 
-    # many of the objects in the windows operating system, regardless ofwhen/how they were 
-    # created over the years, through the WMI Scripting API.Do a Google search and briefly learn about WMI classes and accessing WMI using PowerShell.
+    # Bios Information
+    $Info += "==================================="
+    $Info += "=         BIOS Information        ="
+    $Info += "==================================="
+    $Info += Get-WmiObject Win32_bios -ComputerName $ip
 
-# Your script should be as comprehensiveas possible.
-# Example information may include BIOS information, programs installed on the computer, current programs running, accounts that exist on the computer, etc.
-# While the script by default should run on the machine where it is executed, it should also be able to be executed for a remote machine(assuming you have rights).
+    # Programs installed on the computer
+    $Info += "==================================="
+    $Info += "=  Programs Installed On Computer ="
+    $Info += "==================================="
+    $Info += Get-WmiObject Win32_product -ComputerName $ip
 
+    # current programs running
+    $Info += "==================================="
+    $Info += "=   Programs Running On Computer  ="
+    $Info += "==================================="
+    $Info += -Query "select * from win32_service where State='Running'" -ComputerName $ip |
+        Format-List -Property PSComputerName, Name, ExitCode, Name, ProcessID, StartMode, State, Status
 
-# Your script should be able to be called from a PowerShellconsole and appropriate parameters passed to it.
-# Feel free to usethe internet to find potential solutions;however, you should not copy scripts exactly as you find them online.  You should write them yourself but can be guided by what you find. 
-# Make sure that you comment your script.
-# Your script should save the outputof the information that it gathersto a file.You can determine the type and organization of the file.
+    # accounts that exist on the computer
+    $Info += "==================================="
+    $Info += "=       Accounts On Computer      ="
+    $Info += "==================================="
+    $Info += Get-WmiObject Win32_UserAccount -ComputerName $ip
 
-
-
-
-
-# Pass parameters into powershell
-
-param([string]$ipaddress="127.0.0.1", [switch]$ImSpecial)
-
-
-# Bios Information
-
-Get-WmiObject win32_bios
-
-
-# Programs installed on the computer
-
-
-
-
-# current programs running
-
-
-
-
-# accounts that exist on the computer
-
-
-
-
+    #output information to .csv
+    $Info | Out-File -FilePath .\outputComputerInformation.txt
+}
